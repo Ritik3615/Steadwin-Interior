060fa3542a1bc612143a37a4bd3b1d8f63080b42
Ritik sharma <rs1105312@gmail.com>
2025-11-17 11:12:47 +0530

    sidebaar oversize fixing

--- src/Components/ConatctSidebaar.jsx
+++ src/Components/ConatctSidebaar.jsx
@@ -102,7 +102,7 @@ function ContactSidebar() {
 
         {/* WhatsApp (middle) */}
         <div
-          className="relative flex justify-center items-center h-[70px] md:h-24 bg-[#2fea70f0] cursor-pointer"
+          className="relative flex justify-center items-center h-[70px] md:h-[33%] bg-[#2fea70f0] cursor-pointer"
           onMouseEnter={() => setIsHoveringWhatsApp(true)}
           onMouseLeave={() => setIsHoveringWhatsApp(false)}
         >
